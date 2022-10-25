fe646a2588622e7b451aa67eb7a1b268a463ade9
Alex Braga <contato@alexbraga.com.br>
2022-10-25 00:21:10 -0300

    Implement business details form fields

--- src/components/BusinessDetailsForm.js
+++ src/components/BusinessDetailsForm.js
@@ -1,10 +1,34 @@
 import React from 'react'
 import TextField from "@mui/material/TextField";
 
-function BusinessDetailsForm() {
+function BusinessDetailsForm(props) {
   return (
-    <div>BusinessDetailsForm</div>
-  )
+    <div>
+      <TextField
+        sx={{ marginBottom: 2 }}
+        label="Nome do Empreendimento"
+        name="businessName"
+        onChange={props.handleChange}
+        value={props.formData.businessName}
+      />
+      <br />
+      <TextField
+        sx={{ marginBottom: 2 }}
+        label="CNPJ"
+        name="cnpj"
+        onChange={props.handleChange}
+        value={props.formData.cnpj}
+      />
+      <br />
+      <TextField
+        sx={{ marginBottom: 2 }}
+        label="Valor de Referência"
+        name="referenceValue"
+        onChange={props.handleChange}
+        value={props.formData.referenceValue}
+      />
+    </div>
+  );
 }
 
 export default BusinessDetailsForm
