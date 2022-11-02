59b0bcca50376cb5417242afcc342a9aeaab2479
Alex Braga <contato@alexbraga.com.br>
2022-11-02 15:34:19 -0300

    Add alerts and adjust FormControl height

--- src/components/CoverageFactorForm.js
+++ src/components/CoverageFactorForm.js
@@ -3,6 +3,8 @@ import Radio from "@mui/material/Radio";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import FormControl from "@mui/material/FormControl";
 import FormLabel from "@mui/material/FormLabel";
+import Alert from "@mui/material/Alert";
+import Stack from "@mui/material/Stack";
 import { Coverage } from "../factors";
 
 function CoverageFactorForm(props) {
@@ -30,12 +32,32 @@ function CoverageFactorForm(props) {
 
   return (
     <div>
-      <FormControl>
+      <FormControl sx={{ height: "120px" }}>
         <FormLabel id="demo-radio-buttons-group-label">
           Área de Interferência
         </FormLabel>
         {getOptions()}
       </FormControl>
+
+      <Stack sx={{ width: "60%", margin: "auto" }} spacing={2}>
+        <div className="alert">
+          <Alert severity="info">
+            O Fator de Abrangência é o critério que permite avaliar a
+            distribuição espacial do comprometimento do meio ambiente pelo
+            empreendimento
+          </Alert>
+        </div>
+        <div>
+          <Alert severity="info">
+            "Entende-se por: (1) área de interferência direta - até 10Km da
+            linha perimétrica da área principal do empreendimento, onde os
+            impactos incidem de forma primária; e (2) área de interferência
+            indireta - abrangência regional ou da bacia hidrográfica na qual se
+            insere o empreendimento, onde os impactos incidem de forma
+            secundária ou terciária."
+          </Alert>
+        </div>
+      </Stack>
     </div>
   );
 }
