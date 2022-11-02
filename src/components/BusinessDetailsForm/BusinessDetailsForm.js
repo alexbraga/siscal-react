ba4f40583fccb8a0241266db0e8e741a3ae09a66
Alex Braga <contato@alexbraga.com.br>
2022-11-02 15:25:11 -0300

    Add alerts and fix referenceValue input

--- src/components/BusinessDetailsForm/BusinessDetailsForm.js
+++ src/components/BusinessDetailsForm/BusinessDetailsForm.js
@@ -1,6 +1,9 @@
-import React from 'react'
+import React from "react";
 import TextField from "@mui/material/TextField";
 import CurrencyInput from "@ericblade/react-currency-input";
+import Alert from "@mui/material/Alert";
+import AlertTitle from "@mui/material/AlertTitle";
+import Stack from "@mui/material/Stack";
 import "./styles.css";
 
 function BusinessDetailsForm(props) {
@@ -23,7 +26,8 @@ function BusinessDetailsForm(props) {
       />
       <br />
 
-      <span className="currencyinput">
+      <p>Valor de Referência</p>
+      <span className="currencyinput" style={{ marginBottom: "20px" }}>
         R${" "}
         <CurrencyInput
           name="referenceValue"
@@ -34,8 +38,36 @@ function BusinessDetailsForm(props) {
           value={props.formData.referenceValue}
         />
       </span>
+
+      <Stack sx={{ width: "60%", margin: "auto" }} spacing={2}>
+        <div className="alert">
+          <Alert severity="warning">
+            <AlertTitle>IMPORTANTE</AlertTitle>O Valor de Referência do
+            empreendimento deverá ser informado por profissional legalmente
+            habilitado e estará sujeito à revisão, por parte do órgão
+            competente, impondo-se ao profissional responsável e ao empreendedor
+            as sanções administrativas, civis e penais, nos termos da lei, pela
+            falsidade da informação.
+          </Alert>
+        </div>
+        <div>
+          <Alert severity="info">
+            De acordo com o Decreto Estadual nº 45.175, de 2009 (art. 1º, IV) o
+            Valor de Referência é o somatório dos investimentos inerentes à
+            implantação do empreendimento, excluindo-se os investimentos
+            referentes aos planos, projetos, programas e condicionantes exigidos
+            no procedimento de licenciamento ambiental para mitigação de
+            impactos, os custos de análise do licenciamento ambiental,
+            investimentos que possibilitem alcançar níveis de qualidade
+            ambiental superiores aos exigidos, bem como os encargos e custos
+            incidentes sobre o financiamento do empreendimento, inclusive os
+            relativos às garantias, e os custos com apólices e prêmios de
+            seguros pessoais e reais.
+          </Alert>
+        </div>
+      </Stack>
     </div>
   );
 }
 
-export default BusinessDetailsForm
+export default BusinessDetailsForm;
